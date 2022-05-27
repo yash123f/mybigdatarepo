78f2a2c3df930b551e031d65c4fb054278a8280b
rama <rama@gmail.com>
2022-05-27 11:14:12 +0000

    c1

---
+++ p1.py
@@ -0,0 +1,12 @@
+class MyNumbers:
+   def __iter__(self):
+     self.a=1
+     return self
+   def __next__(self):
+     x=self.a
+     self.a+=1
+     return x
+myclass=MyNumbers()
+myiter=iter(myclass)
+for x in range(10):
+    print(next(myiter))
